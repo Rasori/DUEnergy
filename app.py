98bab34a5521bee32eb5883792d19de95a86949e
Rasori <koskienem@gmail.com>
2020-05-01 15:05:50 +0300

    Automated resolution change and quick range selectors are implemented.
    The main graph now changes x-axis resolution automatically based on the displayed x-axis range. The Time Frame and Resolution radio buttons will be removed in the future but are now left for testing purposes.
    
    Range selector buttons are now implemented and are working with automated resolution change. They calculate the time frame based on last data entry not the current date.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import pandas as pd
+import datetime as dt
+import sqlite3
+from _sqlite3 import Error
+from pathlib import Path
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 import plotly.graph_objs as go
-from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
-from pathlib import Path
-from _sqlite3 import Error
-import sqlite3
-import datetime as dt
 
 
 def form_graph(resolution, d=0, temperature=False, timeframe=None):
@@ -30,51 +29,74 @@ def form_graph(resolution, d=0, temperature=False, timeframe=None):
         begin = (end - dt.timedelta(days=d)).strftime('%Y-%m-%d')
 
     # resolution parameter is searched from timestring
-    timestring = {'Hour': '%Y-%m-%dT%H:00', 'Day hours': '%H', 'Weekday': '%w', 'Day': '%Y-%m-%d', 'Week': '%Y/%W', 'Month': '%Y-%m', 'Year': '%Y'}
+    timestring = {'Hour': '%Y-%m-%dT%H:00',
+                  'Day hours': '%H',
+                  'Weekday': '%w',
+                  'Day': '%Y-%m-%d',
+                  'Week': '%Y-W%W',
+                  'Month': '%Y-%m',
+                  'Year': '%Y'}
 
     df = dataparser(f"{Path(__file__).parent.resolve()}/db/energy_consumption.db",
                     f"SELECT "
-                    f"strftime('{timestring.get(resolution)}',Aikaväli) as TimeFrame, SUM(Kulutus) as Consumption "
-                    f"FROM 'Energy' WHERE Kulutus > 0 AND '{end}' >= Aikaväli AND '{begin}' <= Aikaväli GROUP  BY TimeFrame")
+                    f"strftime('{timestring.get(resolution)}',Aikaväli) as Date, SUM(Kulutus) as Consumption "
+                    f"FROM 'Energy' WHERE Kulutus > 0 AND '{end}' >= Aikaväli AND '{begin}' <= Aikaväli GROUP  BY Date")
 
     dfT = dataparser(f"{Path(__file__).parent.resolve()}/db/energy_consumption.db",
                     f"SELECT "
-                    f"strftime('{timestring.get('Day')}',Aikaväli) as TimeFrame, SUM(Lämpötila) as Temperature "
-                    f"From 'Energy' WHERE '{end}' >= Aikaväli AND '{begin}' <= Aikaväli GROUP  BY TimeFrame")
+                    f"strftime('{timestring.get('Day')}',Aikaväli) as Date, SUM(Lämpötila) as Temperature "
+                    f"From 'Energy' WHERE '{end}' >= Aikaväli AND '{begin}' <= Aikaväli GROUP  BY Date")
 
     if resolution == 'Weekday':  # If Weekdays are set as a resolution x-axis values are replaced with weekdays' names
-        weekday = {'0': 'Sunday', '1': 'Monday', '2': 'Tuesday', '3': 'Wednesday', '4': 'Thursday', '5': 'Friday', '6': 'Saturday'}
-        df['TimeFrame'] = df['TimeFrame'].replace(to_replace=weekday, value=None)
+        weekday = {'0': 'Sunday',
+                   '1': 'Monday',
+                   '2': 'Tuesday',
+                   '3': 'Wednesday',
+                   '4': 'Thursday',
+                   '5': 'Friday',
+                   '6': 'Saturday'}
 
-    """figure = make_subplots(specs=[[{"secondary_y": True}]])
-
-    figure.add_trace(
-        go.Bar(x=df['TimeFrame'], y=df['Consumption'], name="Consumption",  yaxis='y1'),
-        secondary_y=False)
-
-    figure.add_trace(
-        go.Scatter(x=dfT['TimeFrame'], y=dfT['Temperature'], name="Temperature", yaxis='y2'),
-        secondary_y=True,)
-
-    figure.update_layout(
-        xaxis=dict(title=resolution),
-        yaxis=dict(title="Cosumption [kWh]", domain=[0.2, 1]),
-        yaxis2=dict(title="Temperature", side="right"))"""
+        df['Date'] = df['Date'].replace(to_replace=weekday, value=None)
 
     figure = {
         'data': [{
-            'x': df['TimeFrame'],
+            'x': df['Date'],
             'y': df['Consumption'],
             'type': 'bar',
             'name': 'Consumption'}],
         'layout': {
             'title': 'Your energy consumption',
-            'xaxis': {'title': resolution},
-            'yaxis': {'title': 'Consumption [kWh]'},
-            'transition': {'duration': 500, 'easing': 'cubic-in-out'}}}
+            'clickmode': 'event',
+            'uirevision': 'no reset of zoom',
+            'xaxis': {'title': 'Date',
+                      'rangeselector': dict(
+                          buttons=list([
+                            dict(count=1,
+                                 label="D",
+                                 step="day",
+                                 stepmode="backward"),
+                            dict(count=7,
+                                 label="W",
+                                 step="day",
+                                 stepmode="backward"),
+                            dict(count=1,
+                                 label="M",
+                                 step="month",
+                                 stepmode="backward"),
+                            dict(count=6,
+                                 label="6 M",
+                                 step="month",
+                                 stepmode="backward"),
+                            dict(count=1,
+                                 label="Y",
+                                 step="year",
+                                 stepmode="backward"),
+                            dict(step="all")])),
+                      'type': 'date'},
+            'yaxis': {'title': 'Consumption [kWh]'}}}
 
-    if temperature and resolution == 'Hour' or temperature and resolution == 'Day':
-        figure.get('data').append(go.Scatter(x=dfT['TimeFrame'], y=dfT['Temperature'], name='Temperature'))
+    if temperature and resolution == 'Hour' or temperature and resolution == 'Day':  # checks the conditions for temperature trace
+        figure.get('data').append(go.Scatter(x=dfT['Date'], y=dfT['Temperature'], name='Temperature'))
     return figure
 
 
@@ -90,7 +112,7 @@ def main():
                 html.H1(children='DUEnergy', style={'textAlign': 'center'}),
                 html.H3('For those that are interested in their energy consumption habits', style={'textAlign': 'center'})]),
 
-        dcc.Graph(id='main-graph', figure=form_graph('Day', 30)),  # Main Graph
+        dcc.Graph(id='main-graph', figure=form_graph('Day', 30), ),  # Main Graph
 
 
         #  Radio items ETC.
@@ -98,50 +120,71 @@ def main():
             html.Div(children=[
                 html.Label('Time frame:'),
                 dcc.RadioItems(id='Days',
-                    options=[
-                        {'label': 'Last day', 'value': 1},
-                        {'label': 'Last week', 'value': 7},
-                        {'label': 'Last month', 'value': 30},
-                        {'label': 'Last year', 'value': 365},
-                        {'label': 'Everything', 'value': 10*365}],
-                        value=30)], className="two columns")]),
+                               options=[
+                                    {'label': 'Last day', 'value': 1},
+                                    {'label': 'Last week', 'value': 7},
+                                    {'label': 'Last month', 'value': 30},
+                                    {'label': 'Last year', 'value': 365},
+                                    {'label': 'Everything', 'value': 10*365}],
+                               value=10*365)], className="two columns")]),
 
         html.Div(children=[
             html.Div(children=[
                 html.Label('Resolution:'),
                 dcc.RadioItems(id='Resolution',
-                    options=[
-                        {'label': 'Weekdays', 'value': 'Weekday'},
-                        {'label': 'Day hours', 'value': 'Day hours'},
-                        {'label': 'Hourly', 'value': 'Hour'},
-                        {'label': 'Daily', 'value': 'Day'},
-                        {'label': 'Weekly', 'value': 'Week'},
-                        {'label': 'Monthly', 'value': 'Month'},
-                        {'label': 'Yearly', 'value': 'Year'}],
-                        value='Day')], className="two columns")]),
+                               options=[
+                                    {'label': 'Weekdays', 'value': 'Weekday'},
+                                    {'label': 'Day hours', 'value': 'Day hours'},
+                                    {'label': 'Hourly', 'value': 'Hour'},
+                                    {'label': 'Daily', 'value': 'Day'},
+                                    {'label': 'Weekly', 'value': 'Week'},
+                                    {'label': 'Monthly', 'value': 'Month'},
+                                    {'label': 'Yearly', 'value': 'Year'}],
+                               value='Day')], className="two columns")]),
 
         html.Div(children=[
             html.Div(children=[
                 html.Label('Additional options:'),
                 dcc.Checklist(id='Options',
-                    options=[
-                        {'label': 'Show temperature', 'value': 'Temp'},
-                        {'label': 'Compare two time frames', 'value': 'c'}],
-                    value=[])], className="three columns")], className="row")])
+                              options=[
+                                {'label': 'Show temperature', 'value': 'Temp'},
+                                {'label': 'Compare two time frames', 'value': 'c'}],
+                              value=[])], className="three columns")], className="row")])
 
     @app.callback(  # Main Graph update callback
         Output('main-graph', 'figure'),
-        [Input('Resolution', 'value'), Input('Days', 'value'), Input('Options', 'value')])
-    def update_graph(resolution, days, options):
-        if 'Temp' in options:
+        [Input('Resolution', 'value'), Input('Days', 'value'), Input('Options', 'value'), Input('main-graph', 'relayoutData')])
+    def update_graph(resolution, days, options, relayoutData):
+
+        if 'Temp' in options:  # Sets temperature plotting True if check box is checked
             temperature = True
         else:
             temperature = False
 
+        # Handels the resolution change when zoomed
+        if type(relayoutData) == dict:
+            if 'autosize' not in relayoutData and 'xaxis.autorange' not in relayoutData:
+                fmt = '%Y-%m-%d %H'
+                timeframe = dt.datetime.strptime(relayoutData['xaxis.range[1]'].split(':')[0], fmt) - dt.datetime.strptime(
+                    relayoutData['xaxis.range[0]'].split(':')[0], fmt)
+
+                if timeframe.days < 2*365:
+                    resolution = 'Year'
+                if timeframe.days < 765:
+                    resolution = 'Month'
+                #if timeframe.days < 7*6:
+                #    resolution = 'Week'
+                if timeframe.days <= 62:
+                    resolution = 'Day'
+                if timeframe.days <= 7:
+                    resolution = 'Hour'
+        else:
+            resolution = 'Day'
+
         figure = form_graph(resolution, days, temperature=temperature)
         return figure
 
-    app.run_server(host='0.0.0.0', port=8000, debug=True)
+    app.run_server(host='0.0.0.0', port=8000, debug=True)  # Starts the flask server
 
 
 def connect_db(db_file):
